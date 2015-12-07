ea5e78579c6f8b416b1eed8aa1576cff7bf36ff6
ifabiancc <metalicarrion@gmail.com>
2015-12-06 23:21:47 -0500

    Adicionados filtros de busqueda

--- apps/frontend/modules/album/actions/actions.class.php
+++ apps/frontend/modules/album/actions/actions.class.php
@@ -8,81 +8,89 @@
  * @author     Your name here
  * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
  */
-class albumActions extends sfActions
-{
-  public function executeIndex(sfWebRequest $request)
-  {
-      $this->filters = new AlbumFormFilter();
-    $this->albums = Doctrine_Core::getTable('Album')
-      ->createQuery('a')
-      ->execute();
-  }
+class albumActions extends sfActions {
 
-  public function executeNew(sfWebRequest $request)
-  {
-    $this->form = new AlbumForm();
-  }
-
-  public function executeCreate(sfWebRequest $request)
-  {
-    $this->forward404Unless($request->isMethod(sfRequest::POST));
-
-    $this->form = new AlbumForm();
-
-    $this->processForm($request, $this->form);
-
-    $this->setTemplate('new');
-  }
-
-  public function executeEdit(sfWebRequest $request)
-  {
-    $this->forward404Unless($album = Doctrine_Core::getTable('Album')->find(array($request->getParameter('id'))), sprintf('Object album does not exist (%s).', $request->getParameter('id')));
-    $this->form = new AlbumForm($album);
-  }
-
-  public function executeUpdate(sfWebRequest $request)
-  {
-    $this->forward404Unless($request->isMethod(sfRequest::POST) || $request->isMethod(sfRequest::PUT));
-    $this->forward404Unless($album = Doctrine_Core::getTable('Album')->find(array($request->getParameter('id'))), sprintf('Object album does not exist (%s).', $request->getParameter('id')));
-    $this->form = new AlbumForm($album);
-
-    $this->processForm($request, $this->form);
-
-    $this->setTemplate('edit');
-  }
-
-  public function executeDelete(sfWebRequest $request)
-  {
-    $request->checkCSRFProtection();
-
-    $this->forward404Unless($album = Doctrine_Core::getTable('Album')->find(array($request->getParameter('id'))), sprintf('Object album does not exist (%s).', $request->getParameter('id')));
-    $album->delete();
-
-    $this->redirect('album/index');
-  }
-
-  protected function processForm(sfWebRequest $request, sfForm $form)
-  {
-    $form->bind($request->getParameter($form->getName()), $request->getFiles($form->getName()));
-    if ($form->isValid())
-    {
-      $album = $form->save();
-
-      $this->redirect('album/edit?id='.$album->getId());
+    public function executeIndex(sfWebRequest $request) {
+        $this->filter = new AlbumFormFilter();
+        if ($request->getParameter($this->filter->getName())) {
+            $this->filter->bind($request->getParameter($this->filter->getName()));
+            if ($this->filter->isValid()) {
+                $this->albums = $this->filter->getQuery()->execute();
+            } else {
+                $this->albums = Doctrine_Core::getTable('Album')
+                        ->createQuery('a')
+                        ->execute();
+            }
+        } else {
+            $this->albums = Doctrine_Core::getTable('Album')
+                    ->createQuery('a')
+                    ->execute();
+        }
+//        $this->filters = new AlbumFormFilter();
+//        $this->albums = Doctrine_Core::getTable('Album')
+//                ->createQuery('a')
+//                ->execute();
     }
-  }
-  
-  public function executeAddStock(sfWebRequest $request) {
-      $this->forward404Unless($album = Doctrine_Core::getTable('Album')->find(array($request->getParameter('id'))), sprintf('Object album does not exist (%s).', $request->getParameter('id')));
-      $album->setStock($album->getStock() + 1);
-      $album->save();
-      return $this->renderText(json_encode(array('success'=>true, "stock"=>$album->getStock())));
-  }
-  
-  public function executeRemoveStock(sfWebRequest $request) {
-      $this->forward404Unless($album = Doctrine_Core::getTable('Album')->find(array($request->getParameter('id'))), sprintf('Object album does not exist (%s).', $request->getParameter('id')));
-      $album->setStock($album->getStock() - 1);
-      $album->save();
-      return $this->renderText(json_encode(array('success'=>true, "stock"=>$album->getStock())));
-  }
+
+    public function executeNew(sfWebRequest $request) {
+        $this->form = new AlbumForm();
+    }
+
+    public function executeCreate(sfWebRequest $request) {
+        $this->forward404Unless($request->isMethod(sfRequest::POST));
+
+        $this->form = new AlbumForm();
+
+        $this->processForm($request, $this->form);
+
+        $this->setTemplate('new');
+    }
+
+    public function executeEdit(sfWebRequest $request) {
+        $this->forward404Unless($album = Doctrine_Core::getTable('Album')->find(array($request->getParameter('id'))), sprintf('Object album does not exist (%s).', $request->getParameter('id')));
+        $this->form = new AlbumForm($album);
+    }
+
+    public function executeUpdate(sfWebRequest $request) {
+        $this->forward404Unless($request->isMethod(sfRequest::POST) || $request->isMethod(sfRequest::PUT));
+        $this->forward404Unless($album = Doctrine_Core::getTable('Album')->find(array($request->getParameter('id'))), sprintf('Object album does not exist (%s).', $request->getParameter('id')));
+        $this->form = new AlbumForm($album);
+
+        $this->processForm($request, $this->form);
+
+        $this->setTemplate('edit');
+    }
+
+    public function executeDelete(sfWebRequest $request) {
+        $request->checkCSRFProtection();
+
+        $this->forward404Unless($album = Doctrine_Core::getTable('Album')->find(array($request->getParameter('id'))), sprintf('Object album does not exist (%s).', $request->getParameter('id')));
+        $album->delete();
+
+        $this->redirect('album/index');
+    }
+
+    protected function processForm(sfWebRequest $request, sfForm $form) {
+        $form->bind($request->getParameter($form->getName()), $request->getFiles($form->getName()));
+        if ($form->isValid()) {
+            $album = $form->save();
+
+            $this->redirect('album/edit?id=' . $album->getId());
+        }
+    }
+
+    public function executeAddStock(sfWebRequest $request) {
+        $this->forward404Unless($album = Doctrine_Core::getTable('Album')->find(array($request->getParameter('id'))), sprintf('Object album does not exist (%s).', $request->getParameter('id')));
+        $album->setStock($album->getStock() + 1);
+        $album->save();
+        return $this->renderText(json_encode(array('success' => true, "stock" => $album->getStock())));
+    }
+
+    public function executeRemoveStock(sfWebRequest $request) {
+        $this->forward404Unless($album = Doctrine_Core::getTable('Album')->find(array($request->getParameter('id'))), sprintf('Object album does not exist (%s).', $request->getParameter('id')));
+        $album->setStock($album->getStock() - 1);
+        $album->save();
+        return $this->renderText(json_encode(array('success' => true, "stock" => $album->getStock())));
+    }
+
 }
--- apps/frontend/modules/album/templates/indexSuccess.php
+++ apps/frontend/modules/album/templates/indexSuccess.php
@@ -1,5 +1,20 @@
 <h1>Albums List</h1>
-
+<div class="row col-md-12">
+    <form action="" method="post">
+        <?php echo $filter['artist_id']->renderLabel() ?>
+        <?php echo $filter['artist_id']->renderError() ?>
+        <?php echo $filter['artist_id'] ?>
+        <?php echo $filter['genre_id']->renderLabel() ?>
+        <?php echo $filter['genre_id']->renderError() ?>
+        <?php echo $filter['genre_id'] ?>
+        <?php echo $filter['year_production_id']->renderLabel() ?>
+        <?php echo $filter['year_production_id']->renderError() ?>
+        <?php echo $filter['year_production_id'] ?>
+        <?php echo $filter['_csrf_token']->render() ?>
+        <?php // echo $filter ?>
+        <button class="btn btn-success">Filtrar</button>
+    </form>
+</div>
 <table class="table table-bordered">
     <thead>
         <tr>
@@ -25,7 +40,7 @@
                 <td><img width="30px" src="../uploads/album/<?php echo $album->getImage() ?>" /></td>
                 <td><?php echo $album->getPrice() ?></td>
                 <?php if ($sf_user->isAuthenticated()): ?>
-                <td><button class="btn btn-success" onclick="removeStock(<?php echo $album->getId() ?>)"><i class="fa fa-minus"></i></button><label id="stock-<?php echo $album->getId() ?>"><?php echo $album->getStock() ?></label><button onclick="addStock(<?php echo $album->getId() ?>)" class="btn btn-success"><i class="fa fa-plus"></i></button></td>
+                    <td><button class="btn btn-success" onclick="removeStock(<?php echo $album->getId() ?>)"><i class="fa fa-minus"></i></button><label id="stock-<?php echo $album->getId() ?>"><?php echo $album->getStock() ?></label><button onclick="addStock(<?php echo $album->getId() ?>)" class="btn btn-success"><i class="fa fa-plus"></i></button></td>
                     <td><a href="<?php echo url_for('album/edit?id=' . $album->getId()) ?>" class="btn btn-primary"><i class="fa fa-pencil"></i> Edit</a>
                         <a href="<?php echo url_for('album/edit?id=' . $album->getId()) ?>" class="btn btn-danger"><i class="fa fa-minus-circle"></i> Delete</a></td>
                 <?php endif ?>
@@ -35,7 +50,7 @@
 </table>
 <?php if ($sf_user->isAuthenticated()): ?>
     <a href="<?php echo url_for('album/new') ?>" class="btn btn-success"><i class="fa fa-plus-circle"></i> New</a>
-    <?php endif
+<?php endif
 ?>
 <script>
     function addStock(id) {
@@ -44,23 +59,23 @@
             method: "POST",
             data: {id: id},
             dataType: "json",
-            success: function(data){
-                if(data.success){
-                    $("#stock-"+id).text(data.stock);    
+            success: function (data) {
+                if (data.success) {
+                    $("#stock-" + id).text(data.stock);
                 }
             }
         })
     }
-    
+
     function removeStock(id) {
         $.ajax({
             url: "<?php echo url_for('album/removeStock') ?>",
             method: "POST",
             data: {id: id},
             dataType: "json",
-            success: function(data){
-                if(data.success){
-                    $("#stock-"+id).text(data.stock);    
+            success: function (data) {
+                if (data.success) {
+                    $("#stock-" + id).text(data.stock);
                 }
             }
         })
